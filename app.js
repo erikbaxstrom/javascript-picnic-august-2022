1300d97b8204ee20a1e58dee5709df7a1c519d5a
Erik M Baxstrom <baxstrem@gmail.com>
2022-08-11 18:18:25 -0700

    makes icons clickable

---
+++ app.js
@@ -0,0 +1,27 @@
+//get DOM elements
+const soda = document.getElementById('soda');
+const cheese = document.getElementById('cheese');
+const grapes = document.getElementById('grapes');
+const bread = document.getElementById('bread');
+const watermelon = document.getElementById('watermelon');
+// Add event listener
+soda.addEventListener('click', () => {
+    // add the class
+    soda.classList.toggle('picked');
+});
+
+cheese.addEventListener('click', () => {
+    cheese.classList.toggle('picked');
+});
+
+grapes.addEventListener('click', () => {
+    grapes.classList.toggle('picked');
+});
+
+bread.addEventListener('click', () => {
+    bread.classList.toggle('picked');
+});
+
+watermelon.addEventListener('click', () => {
+    watermelon.classList.toggle('picked');
+});
